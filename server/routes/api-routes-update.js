5de2eca944e426aadfef8a87db0efb0c1bc5261f
dinhlegaulois2 <dinh.hu87@yahoo.com>
2018-04-14 06:14:21 -0400

    First Update

--- server/routes/api-routes-update.js
+++ server/routes/api-routes-update.js
@@ -1,1 +1,9 @@
+var Address = require('../data/address.js');
+var Branch = require('../data/branch.js');
+var BranchAddress = require('../data/branchAddress.js');
+var Customer = require('../data/customer.js');
+var CustomerAddress = require('../data/customerAddress.js');
+var Rental = require('../data/rental.js');
+var Vehicle = require('../data/vehicle.js');
+
 module.exports = function (app) {}

44f1709b8f2baa85a96fe5f732e0e15f32c0f2d8
dinhlegaulois2 <dinh.hu87@yahoo.com>
2018-04-18 01:15:23 -0400

    Read: Done

--- server/routes/api-routes-read.js
+++ server/routes/api-routes-read.js
@@ -6,4 +6,84 @@ var CustomerAddress = require('../data/customerAddress.js');
 var Rental = require('../data/rental.js');
 var Vehicle = require('../data/vehicle.js');
 
-module.exports = function (app) {}
+module.exports = function (app) {
+    app.get("/api/get/vehicle/:id", (req, res, next) => {
+        Vehicle.find({ _id: req.params.id }).then(data => res.status(200).json(data))
+            .catch(next)
+    })
+
+    app.get("/api/get/branch/:id", (req, res, next) => {
+        let result = {}
+        Branch.find({ _id: req.params.id }).then(data => {
+            if (data.length > 0) {
+                result.branch = data
+                BranchAddress.find({ branchId: data._id }).then(data => {
+                    if (data.length == 0)
+                        res.status(400).json({ msg: "BranchAddress Not Found" })
+                    else {
+                        Address.find({ _id: data.addressId }).then(data => {
+                            if (data.length > 0) {
+                                result.address = data
+                                res.status(200).json(result)
+                            }
+                            else res.status(400).json({ msg: "Address Not Found" })
+                        }).catch(next)
+                    }
+                }).catch(next)
+            }
+            else res.status(400).json({ msg: "Branch Not Found" })
+        }).catch(next)
+    })
+
+    app.get("/api/get/customer/:id", (req, res, next) => {
+        let result = {}
+        Customer.find({ _id: req.params.id }).then(data => {
+            if (data.length > 0) {
+                result.customer = data
+                customerAddress.find({ customerId: data._id }).then(data => {
+                    if (data.length == 0)
+                        res.status(400).json({ msg: "BranchAddress Not Found" })
+                    else {
+                        Address.find({ _id: data.addressId }).then(data => {
+                            if (data.length > 0) {
+                                result.address = data
+                                res.status(200).json(result)
+                            }
+                            else res.status(400).json({ msg: "Address Not Found" })
+                        }).catch(next)
+                    }
+                }).catch(next)
+            }
+            else res.status(400).json({ msg: "Branch Not Found" })
+        }).catch(next)
+    })
+
+    app.get("/api/get/rental/:id", (req, res, next) => {
+        let result = {}
+        Rental.find({ _id: req.params.id }).then(data => {
+            if (data.length > 0) {
+                result.rental = data
+                Vehicle.find({ _id: data.vehiculeId }).then(data => {
+                    if (data.length == 0)
+                        res.status(400).json({ msg: "BranchAddress Not Found" })
+                    else {
+                        Branch.find({ _id: data.branchId }).then(data => {
+                            if (data.length > 0) {
+                                result.branch = data
+                                Customer.find({ _id: data.customerId }).then(data => {
+                                    if (data.length > 0) {
+                                        result.customer = data
+                                        res.status(200).json(result)
+                                    }
+                                    else res.status(400).json({ msg: "Customer Not Found" })
+                                }).catch(next)
+                            }
+                            else res.status(400).json({ msg: "Branch Not Found" })
+                        }).catch(next)
+                    }
+                }).catch(next)
+            }
+            else res.status(400).json({ msg: "Rental Not Found" })
+        }).catch(next)
+    })
+}

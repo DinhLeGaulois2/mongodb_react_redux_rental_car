1fb1c646b3568346da3f1a36416388375339b063
dinhlegaulois2 <dinh.hu87@yahoo.com>
2018-04-18 00:22:46 -0400

    Delete: Done!

--- server/routes/api-routes-delete.js
+++ server/routes/api-routes-delete.js
@@ -6,4 +6,16 @@ var CustomerAddress = require('../data/customerAddress.js');
 var Rental = require('../data/rental.js');
 var Vehicle = require('../data/vehicle.js');
 
-module.exports = function (app) {}
+module.exports = function (app) {
+    app.delete("/api/delete/rental/:id", (req, res, next) => {
+        Rental.delete({ _id: req.params.id })
+        .then(data => res.status(200).json("Delete: Success!"))
+        .catch(next)
+    })
+    
+    app.delete("/api/delete/vehicle/:id", (req, res, next) => {
+        Vehicle.delete({ _id: req.params.id })
+        .then(data => res.status(200).json("Delete: Success!"))
+        .catch(next)
+    })
+}
